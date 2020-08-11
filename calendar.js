b10f56eeef223ab1471b110390919a78d10aed3e
iamhabi <chanvinhong@gmail.com>
2020-08-11 16:21:34 +0900

    Add test that shows date

--- calendar.js
+++ calendar.js
@@ -5,42 +5,53 @@ var firstDay = new Date(now.getFullYear(), now.getMonth()).getDay();
 var month = now.getMonth();
 var today = now.getDate();
 
+let monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let dayArray = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
 var x = 7, y = 5;
 var cnt = prevDate - firstDay + 1;
 
 if (firstDay < 5) y = 4;
 
-document.write("<table>");
-document.write("<tr>");
+drawCalendar();
+var dayinMilliSec = 1000 * 60 * 60 * 24;
+setInterval(drawCalendar, dayinMilliSec);
 
-// Previous Month
-for (i = 0; i < firstDay; i++) {
-    document.write("<td style='background-color: white;'><p>" + cnt + "</p></td>");
-    cnt++;
-}
+function drawCalendar() {
+    document.write("<h1>" + monthArray[now.getMonth()] + " " + today + " " + dayArray[now.getDay()] + "</h1>");
 
-cnt = 1;
-for (i = firstDay; i < x; i++) {
-    document.write("<td style='background-color: white;'><p>" + cnt + "</p></td>");
-    cnt++;
-}
-
-document.write("</tr>");
-
-for (i = 0; i < y; i++) {
+    document.write("<table>");
     document.write("<tr>");
-    
-    for (j = 0; j < x; j++) {
-        if (cnt > lastDate) cnt = 1;
 
-        if (cnt == today) {
-            document.write("<td style='background-color: skyblue;'><p>" + cnt + "</p></td>");
-        } else {
-            document.write("<td style='background-color: white;'><p>" + cnt + "</p></td>");
-        }
+    // Previous Month
+    for (i = 0; i < firstDay; i++) {
+        document.write("<td style='background-color: white;'><p>" + cnt + "</p></td>");
+        cnt++;
+    }
+
+    cnt = 1;
+    for (i = firstDay; i < x; i++) {
+        document.write("<td style='background-color: white;'><p>" + cnt + "</p></td>");
         cnt++;
     }
 
     document.write("</tr>");
+
+    for (i = 0; i < y; i++) {
+        document.write("<tr>");
+        
+        for (j = 0; j < x; j++) {
+            if (cnt > lastDate) cnt = 1;
+
+            if (cnt == today) {
+                document.write("<td style='background-color: skyblue;'><p>" + cnt + "</p></td>");
+            } else {
+                document.write("<td style='background-color: white;'><p>" + cnt + "</p></td>");
+            }
+            cnt++;
+        }
+
+        document.write("</tr>");
+    }
+    document.write("</table>");
 }
-document.write("</table>");
